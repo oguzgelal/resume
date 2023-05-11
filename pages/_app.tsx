ce8656b06b3fd14a76598d8ae86ea93a7de48460
Oguz <o.gelal77@gmail.com>
2023-05-11 11:06:03 +0100

    replace clarity with vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,12 @@
 import "../styles/globals.css";
-import Script from "next/script";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
-      <Script id="clarity">
-        {`(function(c,l,a,r,i,t,y){
-          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-      })(window, document, "clarity", "script", "h282yrwovn");`}
-      </Script>
+      <Analytics />
     </>
   );
 }
