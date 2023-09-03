3e6c2f1c7a873d3debf95d19ddc11819ebc1ce8b
Robert Schäfer <git@roschaefer.de>
2023-09-03 12:04:48 +0200

    refactor: consistency between demo and index page

--- pages/demo.tsx
+++ pages/demo.tsx
@@ -1,7 +1,16 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 import Resume from "../components/Resume";
 import resume from "../data/demo";
 
-const Home: NextPage = () => <Resume resume={resume} />;
+const Home: NextPage = () => (
+  <>
+    <Head>
+      <title>Marko Marković | CV</title>
+    </Head>
+
+    <Resume resume={resume} />
+  </>
+);
 
 export default Home;
